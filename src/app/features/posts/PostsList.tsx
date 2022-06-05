935b900a094ea685c08d9defafa8e49311154e5f
vgtmhl <vgtmhl@gmail.com>
2022-06-05 13:26:13 +0200

    added timeAgo component and logic

--- src/app/features/posts/PostsList.tsx
+++ src/app/features/posts/PostsList.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from '../../store';
 import { selectAllPosts } from './postsSlice'
 import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
     const posts = useAppSelector(selectAllPosts);
@@ -8,7 +9,7 @@ const PostsList = () => {
     return (
         <section>
             <h2>Posts</h2>
-            {posts.map(({ id, title, content, userId }) => (
+            {posts.map(({ id, title, content, userId, date }) => (
                 <article key={id}>
 
                     <h3>{title}</h3>
@@ -16,6 +17,7 @@ const PostsList = () => {
 
                     <p className='postCredit'>
                         <PostAuthor userId={userId} />
+                        <TimeAgo timestamp={date} />
                     </p>
                 </article>
             ))}
--- src/app/features/posts/postsSlice.ts
+++ src/app/features/posts/postsSlice.ts
@@ -1,11 +1,29 @@
 import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../store";
 import { Post } from "./postModels";
+import { sub } from "date-fns";
 
 const initialState = [
-    { id: '1', title: 'Learning Redux toolkit', content: "I've heard good things about Redux Toolkit", userId: '1' },
-    { id: '3', title: 'Redux in plain english', content: "I've translated Redux to English" },
-    { id: '5', title: 'Redux in Japanese', content: "I've translated Redux to Japanese", userId: '0' },
+    {
+        id: '1',
+        title: 'Learning Redux toolkit',
+        content: "I've heard good things about Redux Toolkit",
+        userId: '1',
+        date: sub(new Date(), { minutes: 10 }).toISOString()
+    },
+    {
+        id: '3',
+        title: 'Redux in plain english',
+        content: "I've translated Redux to English",
+        date: sub(new Date(), { minutes: 20 }).toISOString()
+    },
+    {
+        id: '5',
+        title: 'Redux in Japanese',
+        content: "I've translated Redux to Japanese",
+        userId: '0',
+        date: sub(new Date(), { minutes: 30 }).toISOString()
+    },
 ]
 
 const postsSlice = createSlice({
@@ -30,6 +48,7 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
+                        date: new Date().toISOString(),
                         userId
                     }
                 }
