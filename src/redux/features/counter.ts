05599ebea0c09e51e8d310eb2b6211adaf0a7836
vgtmhl <vgtmhl@gmail.com>
2022-06-05 08:33:51 +0200

    TS redux boilerplate

---
+++ src/redux/features/counter.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface CounterState {
+    count: number,
+}
+
+const initialState: CounterState = {
+    count: 0
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment: (state) => { state.count += 1 },
+        decrement: (state) => { state.count -= 1 },
+        reset: (state) => { state.count = 0 },
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.count += action.payload
+        }
+    }
+})
+
+export const { increment, decrement, incrementByAmount, reset } = counterSlice.actions
+export default counterSlice.reducer;
